bff97fec53aca1f2f52a9225df4f077117560159
sergio-banuelos <cain-x@hotmail.com>
2014-09-24 04:29:05 -0500

    cambio del .travis

--- tests/features/steps.py
+++ tests/features/steps.py
@@ -4,8 +4,8 @@ import app.match as m
 
 
 @step(u'Given: "([^"]*)" and "([^"]*)" start a match to "([^"]*)" sets')
-def given_group1_and_group2_start_a_match_to_group3_sets(step, group1, group2, group3):
-    world.match = m.Match(group1,group2,group3)
+def given_g1_and_g2_start_a_match_to_g3_sets(step, g1, g2, g3):
+    world.match = m.Match(g1, g2, g3)
 
 
 @step(u'When: "([^"]*)" and "([^"]*)" start a match to "([^"]*)" sets')
@@ -15,7 +15,7 @@ def when_group1_and_group2_start_a_match_to_group3_sets(step, group1, group2, gr
 
 @step(u'Then: I see score: "([^"]*)"')
 def then_i_see_score_group1(step, group1):
-    assert world.match.score() == group1, 'Error got: ' +world.match.score()
+    assert world.match.score() == group1, 'Error got: ' + world.match.score()
 
 
 @step(u'When: "([^"]*)" won the "([^"]*)" set "([^"]*)"-"([^"]*)"')
@@ -31,5 +31,3 @@ def and_group1_won_the_group2_set_group3_group4(step, group1, group2, group3, gr
 @step(u'Then: The match score is: "([^"]*)"')
 def then_the_match_score_is_group1(step, group1):
     assert False, 'This step must be implemented'
-
-
